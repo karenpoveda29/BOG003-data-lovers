c42ab536a2ad6e7fcf5131d204cf6f966c1946c8
Mayerly Rodriguez <m4ydri@gmail.com>
2021-08-04 14:05:03 -0500

    Crear una función para mostrar una tarjeta pokemon y un ciclo para mostrarlos todos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+const insertarPokemon = (number,type,name,maxCp,img) => {
+    const section = document.getElementById("section");
+    section.insertAdjacentHTML("beforeend", ` 
+        <div class="card-pokemon"> 
+            <img class="image-pokemon" src="${img}">
+            <p class="number">${number}</p>
+            <p class="name">${name}</p>
+            <div class="type">${type}</div>
+            <p class="combat-power">${maxCp}</p>            
+        </div>
+    `); 
+} 
+
+const todosPokemon = () => {
+    const pokemon = data.pokemon; 
+    for(let i= 0; i<pokemon.length; i++){
+        const number = pokemon[i].num;
+        const type = pokemon[i].type;
+        const name = pokemon[i].name;
+        const maxCp = pokemon[i].stats['max-cp'];
+        const img = pokemon[i].img;
+        insertarPokemon(number,type,name,maxCp,img);
+    }
+}
+todosPokemon();
+
+
+
+
+
+
+
+
