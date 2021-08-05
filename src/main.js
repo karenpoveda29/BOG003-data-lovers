44744d9e7b325d41a6265286e3b440f8b5446f68
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-08-05 16:10:22 -0500

    cambiar el nombre del algunas variables y funciones a inglés

--- src/main.js
+++ src/main.js
@@ -3,31 +3,30 @@ import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
 
 
-const insertarPokemon = (number,type,name,maxCp,img) => {
-    const section = document.getElementById("pokemon-cards");
-    section.insertAdjacentHTML("beforeend", ` 
-        <div class="card-pokemon"> 
+const insertPokemon = (number,type,name,maxCp,img) => {
+    const cardsSection = document.getElementById("cards-section");
+    cardsSection.insertAdjacentHTML("beforeend", ` 
+        <div class="pokemon-card"> 
             <img class="image-pokemon" src="${img}">
             <p class="number">${number}</p>
             <p class="name">${name}</p>
             <div class="type">${type}</div>
-            <p class="combat-power">${maxCp}</p>            
-        </div>
-    `); 
+            <p class="combat-power">Max-PC ${maxCp}</p>            
+        </div>`); 
 } 
 
-const todosPokemon = () => {
+const allPokemon = () => {
     const pokemon = data.pokemon; 
     for(let i= 0; i<pokemon.length; i++){
         const number = pokemon[i].num;
-        const type = pokemon[i].type;
+        const type = pokemon[i].type; 
         const name = pokemon[i].name;
         const maxCp = pokemon[i].stats['max-cp'];
         const img = pokemon[i].img;
-        insertarPokemon(number,type,name,maxCp,img);
+        insertPokemon(number,type,name,maxCp,img);
     }
 }
-todosPokemon();
+allPokemon();
 
 
 
