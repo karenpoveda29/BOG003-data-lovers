ff7485925eff548089122c57b8007312774ce692
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-08-13 21:41:21 -0500

    Agregar las funciones para odernar

--- src/data.js
+++ src/data.js
@@ -11,3 +11,25 @@ export const getPercentage = (totalPerType, totalPokemon) => {
   const percentage = parseFloat((100 * totalPerType) / totalPokemon).toFixed(2);
   return percentage;
 };
+
+
+export const sortedAscendent = (pokemons) => {
+  const lowToHigh = pokemons.sort((a, b) => {
+  return  a.stats["max-cp"] - b.stats["max-cp"];
+  });
+  return lowToHigh;
+};
+
+export const sortedDescendent = (pokemons) => {
+  const highToLow = pokemons.sort((a, b) => {
+    return b.stats["max-cp"] - a.stats["max-cp"];
+  });
+  return highToLow;
+}; 
+    
+
+
+
+
+
+
